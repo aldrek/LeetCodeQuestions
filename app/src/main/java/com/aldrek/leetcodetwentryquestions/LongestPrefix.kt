101504c0bd5deca5f8c958d3063ae6a537227c2f
Ahmad salman <ahmadsalman327@gmail.com>
2021-10-04 13:23:47 +0300

    Longest prefix solution

--- app/src/main/java/com/aldrek/leetcodetwentryquestions/LongestPrefix.kt
+++ app/src/main/java/com/aldrek/leetcodetwentryquestions/LongestPrefix.kt
@@ -1,13 +1,32 @@
 package com.aldrek.leetcodetwentryquestions
 
 fun main(){
-    var list = mutableListOf("flower" , "flow" , "flight")
-    var result =  findLongestPrefix(list)
-    println(result)
+    var list: Array<String> = arrayOf("a")
+    var result =  findLongestPrefix(list.toMutableList())
+    var finalResult = result.groupingBy { it }.eachCount().filter { it.value ==  list.size }
+    if( finalResult.keys.isEmpty()){
+       print("")
+    }
+    println(finalResult.keys.toMutableList()[finalResult.keys.size-1])
 }
 
-fun findLongestPrefix(list: MutableList<String>) {
+fun findLongestPrefix(list: MutableList<String>) : MutableList<String> {
+    var prefixArray = mutableListOf<String>()
 
+    list.forEachIndexed { index, item ->
+
+        var endPoint = 1
+        var word = item
+
+        word.toCharArray().forEachIndexed { index, char ->
+            prefixArray.add(word.substring(0,endPoint))
+            endPoint++
+
+        }
+
+    }
+
+    return prefixArray
 
 }
 
